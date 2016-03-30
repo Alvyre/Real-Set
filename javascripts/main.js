5d8ce2f6d26aeafa4079ee8597168c4318619211
Arctic76 <belum76@gmail.com>
2016-03-30 17:12:07 +0200

    Template gallery
    - gallery template + Js slider to compare images.
    - Isotope to be responsive easily
    - model page for DCRAW & Camera Stance (avoiding 404 err)

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,4 +9,25 @@ $(document).ready(function() {
 			preload: false // fixes issue with iframe and IE
 		}
 	});
+
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+	if(document.title == 'Gallery'){
+		$('.grid').isotope({
+  		// options
+  		itemSelector: '.grid-item',
+  		layoutMode: 'fitRows'
+		});
+	}
 });
+
